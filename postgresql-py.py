2f9fb88dbc83cb165d0d1ec30cdeabcb79246fc9
Gustavo <gustavocoderjs@gmail.com>
2025-04-22 01:50:48 -0300

    Conectando Python com PostgreSQL

---
+++ postgresql-py.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+conn = psycopg2.connect(host="localhost", dbname= "curso-psql", 
+                        user="postgres", password="1234", 
+                        port=5432)
+
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS person(
+    id INT PRIMARY KEY, 
+    name VARCHAR (255),
+    age INT,
+    gender CHAR
+    );
+""")
+            
+conn.commit()
+cur.close()
+conn.close()
+
